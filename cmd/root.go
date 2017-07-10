eec16f97a91d519b5b6f521cc1aaf385d700e09d
Evan Tschuy <evan.tschuy@coreos.com>
2017-07-10 11:54:45 -0700

    Clean up formatting Changes the formatting of token name so that tokens are left-padded / center aligned with the tokens lining up.

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/hgfischer/go-otp"
@@ -25,6 +26,20 @@ func getEnvDefault(env string, def string) string {
 	return def
 }
 
+func lengthofLongest(files []os.FileInfo) int {
+	m := 0
+	for _, e := range files {
+		if len(e.Name()) > m {
+			m = len(e.Name())
+		}
+	}
+	return m
+}
+
+func leftPad(str string, length int) string {
+	return strings.Repeat(" ", length-len(str)+1) + str
+}
+
 var RootCmd = &cobra.Command{
 	Use:   "gotp",
 	Short: "one-time password generation tool",
@@ -33,18 +48,19 @@ var RootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		files, _ := ioutil.ReadDir(tokenDir)
 		fmt.Println(time.Now().Format(time.UnixDate))
+		longest := lengthofLongest(files)
 		for _, f := range files {
 			mode := f.Mode()
 			if mode.IsDir() {
 				tk, err := token.ReadToken(f.Name())
 				if err != nil {
-					log.Fatalf("error reading token %s: %s", f.Name(), err)
+					log.Fatalf("error reading token %s: %s", leftPad(f.Name(), longest), err)
 				}
 				if tk.Hotp {
-					fmt.Printf("HOTP: %s\n", tk.Name)
+					fmt.Printf("HOTP: %s\n", leftPad(tk.Name, longest))
 				} else {
 					totp := &otp.TOTP{Secret: tk.Token, IsBase32Secret: true}
-					fmt.Printf("%s: %s\n", tk.Name, totp.Get())
+					fmt.Printf("%s: %s\n", leftPad(tk.Name, longest), totp.Get())
 				}
 			}
 		}
