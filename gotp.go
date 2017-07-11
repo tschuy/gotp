4bdd32610263a6e072c866d520b2ce111ab9941f
Evan Tschuy <evan.tschuy@coreos.com>
2017-07-10 17:08:48 -0700

    add check for gpg-agent < 2 with defined GPG_AGENT_INFO

--- gotp.go
+++ gotp.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 
@@ -8,8 +9,20 @@ import (
 )
 
 func main() {
-	// horrible hack
-	os.Setenv("GPG_AGENT_INFO",
-		"/run/user/"+strconv.FormatInt(int64(os.Getuid()), 10)+"/gnupg/S.gpg-agent:12345:1")
+	if _, err := os.Stat("/run/user/" + strconv.FormatInt(int64(os.Getuid()), 10) + "/gnupg/S.gpg-agent"); os.IsNotExist(err) && os.Getenv("GPG_AGENT_INFO") == "" {
+		fmt.Print("Unable to find gpg-agent; is it running?\n")
+		os.Exit(1)
+	}
+
+	// how can we connect to GPG Agent? there are a few ways:
+	//   * gpg-agent before v2: stored in GPG_AGENT_INFO
+	//   * gpg-agent after v2: stored in "a stable pathname now", according to the Debian docs
+	//		* on systemd, this means /run/user/[uid]/gnupg/S.gpg-agent
+	//		* on other init systems that don't use FHS, let alone other OSes, more research is necessary
+	// https://wiki.debian.org/Teams/GnuPG/UsingGnuPGv2#GPG_AGENT_INFO_variable
+	if os.Getenv("GPG_AGENT_INFO") == "" {
+		os.Setenv("GPG_AGENT_INFO",
+			"/run/user/"+strconv.FormatInt(int64(os.Getuid()), 10)+"/gnupg/S.gpg-agent:12345:1")
+	}
 	cmd.Execute()
 }
