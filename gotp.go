3e390f6bd5d0d706ab8c9efd7422606ce10bdfe3
Evan Tschuy <evan.tschuy@coreos.com>
2017-07-10 17:26:40 -0700

    support three ways for finding the gpg-agent socket

--- gotp.go
+++ gotp.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -8,21 +9,37 @@ import (
 	"github.com/tschuy/gotp/cmd"
 )
 
+var systemdSocket = "/run/user/" + strconv.FormatInt(int64(os.Getuid()), 10) + "/gnupg/S.gpg-agent"
+var homeSocket = os.Getenv("HOME") + "/.gnupg/S.gpg-agent"
+
+// how can we connect to GPG Agent? there are a few ways:
+//   * gpg-agent before v2: stored in GPG_AGENT_INFO
+//   * gpg-agent after v2: stored in "a stable pathname now", according to the Debian docs
+//		* on systemd, this means /run/user/[uid]/gnupg/S.gpg-agent
+//		* in general, $HOME/.gnupg/S.gpg-agent
+// https://wiki.debian.org/Teams/GnuPG/UsingGnuPGv2#GPG_AGENT_INFO_variable
+func setupSocket() error {
+	if os.Getenv("GPG_AGENT_INFO") != "" {
+		return nil
+	}
+	if _, err := os.Stat(systemdSocket); !os.IsNotExist(err) {
+		os.Setenv("GPG_AGENT_INFO", systemdSocket+":12345:1")
+		return nil
+	}
+	if _, err := os.Stat(homeSocket); !os.IsNotExist(err) {
+		os.Setenv("GPG_AGENT_INFO", homeSocket+":12345:1")
+		return nil
+	}
+
+	return errors.New("unable to find gpg-agent")
+}
+
 func main() {
-	if _, err := os.Stat("/run/user/" + strconv.FormatInt(int64(os.Getuid()), 10) + "/gnupg/S.gpg-agent"); os.IsNotExist(err) && os.Getenv("GPG_AGENT_INFO") == "" {
-		fmt.Print("Unable to find gpg-agent; is it running?\n")
+	err := setupSocket()
+	if err != nil {
+		fmt.Println("Could not find gpg-agent; is it running?")
 		os.Exit(1)
 	}
 
-	// how can we connect to GPG Agent? there are a few ways:
-	//   * gpg-agent before v2: stored in GPG_AGENT_INFO
-	//   * gpg-agent after v2: stored in "a stable pathname now", according to the Debian docs
-	//		* on systemd, this means /run/user/[uid]/gnupg/S.gpg-agent
-	//		* on other init systems that don't use FHS, let alone other OSes, more research is necessary
-	// https://wiki.debian.org/Teams/GnuPG/UsingGnuPGv2#GPG_AGENT_INFO_variable
-	if os.Getenv("GPG_AGENT_INFO") == "" {
-		os.Setenv("GPG_AGENT_INFO",
-			"/run/user/"+strconv.FormatInt(int64(os.Getuid()), 10)+"/gnupg/S.gpg-agent:12345:1")
-	}
 	cmd.Execute()
 }
